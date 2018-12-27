7bdcb310a673099601fdad60c4c083d3ecc9d577
Zinfira <ZSafargalieva@gmail.com>
2018-12-26 19:51:50 -0800

    easy 3

---
+++ task_3_easy.py
@@ -0,0 +1,9 @@
+_author_ = 'Safargalieva Zinfira Ismagilovna'
+
+print("Task №3")
+
+age = int(input("Введите Ваш возраст: "))
+if age >= 18:
+    print("Доступ разрешён!")
+else:
+    print("Извините, пользование данным ресурсом только с 18 лет!")
