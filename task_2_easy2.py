bf256b4c2a6f7e4ab3a96a107ebc6266e4dd5f3d
Zinfira <ZSafargalieva@gmail.com>
2018-12-26 19:49:37 -0800

    task 2 easy 2

---
+++ task_2_easy2.py
@@ -0,0 +1,10 @@
+_author_ = 'Safargalieva Zinfira Ismagilovna'
+
+print("Task №2")
+a = input("Введите число a: ")
+b = input("Введите число b: ")
+c = a
+a = b
+b = c
+print("Новое значение: ", a)
+print("Новое значение: ", b)
